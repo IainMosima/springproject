3b55f37865b1b28e261589d7c4c6efc8969df7a3
IainMosima <iainmosima@gmail.com>
2024-04-25 17:15:26 +0300

    setting up project for rest api

--- src/main/java/com/javaproject/springproject/SpringprojectApplication.java
+++ src/main/java/com/javaproject/springproject/SpringprojectApplication.java
@@ -1,7 +1,5 @@
 package com.javaproject.springproject;
 
-import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,18 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringprojectApplication {
 
 	public static void main(String[] args) {
-		var app = new SpringApplication(SpringprojectApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "test"));
-		var ctx = app.run(args);
-		// var ctx = SpringApplication.run(SpringprojectApplication.class, args);
-
-		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
-		System.out.println(myFirstService.tellAStory());
-		System.out.println(myFirstService.getCustomProperty());
+		SpringApplication.run(SpringprojectApplication.class, args);
 		
-		// System.out.println(myFirstService.getJavaVersion());
-		// System.out.println(myFirstService.getOsName());
-		// System.out.println(myFirstService.readProperty());
 	}
 
 }
