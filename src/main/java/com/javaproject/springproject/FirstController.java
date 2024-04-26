d8f6d23dea3031fefebd83fbbb14feb34c5ae6ac
IainMosima <iainmosima@gmail.com>
2024-04-26 18:32:56 +0300

    starting to work with jpa

--- src/main/java/com/javaproject/springproject/FirstController.java
+++ src/main/java/com/javaproject/springproject/FirstController.java
@@ -1,15 +1,13 @@
 package com.javaproject.springproject;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
-    // @GetMapping("/hello")
+    @GetMapping("/hello")
     public String sayHello() {
         return "Hello from my first controller";
     }
@@ -20,22 +18,23 @@ public class FirstController {
         return "Request accepted and message is : " + message;
     }
 
-    @PostMapping("/post-order")
-    public String postOrder(
-            @RequestBody Order order) {
-        return "Request accepted and message is : " + order.toString();
-    }
-
-    // @GetMapping("/hello/{user-name}")
-    public String pathVar(
-            @PathVariable("user-name") String userName) {
-        return "my value = " + userName;
-    }
-
-    @GetMapping("/hello")
-    public String pathVar2(
-            @RequestParam("firstname") String fisrtName,
-            @RequestParam("lastname") String lastName) {
-        return "my value = " + fisrtName + " " + lastName;
-    }
 }
+
+// @PostMapping("/post-order")
+// public String postOrder(
+//         @RequestBody Order order) {
+//     return "Request accepted and message is : " + order.toString();
+// }
+
+// // @GetMapping("/hello/{user-name}")
+// public String pathVar(
+//         @PathVariable("user-name") String userName) {
+//     return "my value = " + userName;
+// }
+
+// @GetMapping("/hello")
+// public String pathVar2(
+//         @RequestParam("firstname") String fisrtName,
+//         @RequestParam("lastname") String lastName) {
+//     return "my value = " + fisrtName + " " + lastName;
+// }
